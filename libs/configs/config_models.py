c95f542e307e863ef1a9e93c13c7c6c5f33336b4
Ded Maroz <dedkamaroz@gmail.com>
2023-09-27 01:54:04 +1000

    Update config_models.py
    corrected the path for the ffhq entries

--- libs/configs/config_models.py
+++ libs/configs/config_models.py
@@ -10,12 +10,12 @@ stylegan2_voxceleb_256 = {
 stylegan2_ffhq_256 = {
 	'image_resolution':			256,
 	'channel_multiplier':		2,
-	'gan_weights':				'/home/stella/Desktop/projects/Finding_Directions_Reenactment/pretrained_models/stylegan_rosinality.pt',
+	'gan_weights':				'./pretrained_models/stylegan_rosinality.pt',
 }
 
 stylegan2_ffhq_1024 = {
 	'image_resolution':			1024,
 	'channel_multiplier':		2,
-	'gan_weights':				'/home/stella/Desktop/projects/Finding_Directions_Reenactment/pretrained_models/stylegan2-ffhq-config-f_1024.pt',
+	'gan_weights':				'./pretrained_models/stylegan2-ffhq-config-f_1024.pt',
 }
 
